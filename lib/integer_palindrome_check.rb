2c4232989e69d767d8148c00e12579228b967824
SelamawitA <Selamawit.ainalem@gmail.com>
2018-04-01 22:02:37 -0700

    added method to check if an integer is a palindrome

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,31 @@
-# Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  size_of_number = 1
+  number_size = number
+  number_for_comparing = number
+  compared_number = 0
+
+  if number == nil
+    return false
+  end
+  if number < 0
+    return false
+  end
+
+  while number_size > 9
+    number_size/=10
+    size_of_number*= 10
+  end
+
+  while number_for_comparing > 0
+    compared_number += ((number_for_comparing%10)*size_of_number)
+    number_for_comparing /= 10
+    size_of_number /=10
+  end
+
+  if compared_number == number
+    return  true
+  else
+    return false
+  end
+
 end
